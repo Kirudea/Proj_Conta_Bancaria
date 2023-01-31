97204f8b492c6dab2e83c7ba9451f84dd2488234
Kirudea <kilderehenriquedp@gmail.com>
2023-01-31 17:26:41 -0300

    Inicializado

---
+++ src/test/java/com/banco/AppTest.java
@@ -0,0 +1,11 @@
+package com.banco;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
